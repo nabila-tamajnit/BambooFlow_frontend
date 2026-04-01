5a09f4b41c293643b0ab63d666e9e6fd9f1d2668
Nabila Tamajnit <n.tamajnit.dev@gmail.com>
2026-04-01 16:19:20 +0200

    change logo

--- src/features/tasks/pages/TastHome.jsx
+++ src/features/tasks/pages/TastHome.jsx
@@ -5,7 +5,7 @@ export const TaskHome = () => {
     return (
         <>
             <section className="py-6 px-12 flex items-center gap-6">
-                <img className="w-40" src="/icons/panda_logo.svg" alt="La grosse tête de Ducky" />
+                <img className="w-40" src="/icons/producktivity_logo.svg" alt="La grosse tête de Ducky" />
                 <h1 className="text-3xl text-main-800 font-chewy">Olala, tu as pleins de tâches à faire !</h1>
             </section>
 
