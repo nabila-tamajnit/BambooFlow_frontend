9e976debe8be6b0156d89616a3af025339db3ed9
Nabila Tamajnit <n.tamajnit.dev@gmail.com>
2026-04-03 14:59:10 +0200

    design: Modify header CSS

--- src/layout/components/Header.jsx
+++ src/layout/components/Header.jsx
@@ -1,37 +1,55 @@
+import { useAtomValue } from 'jotai'
 import { NavLink } from "react-router"
+import { isConnectAtom } from '../../atoms/auth.atom'
+import { BtnLogout } from '../../features/auth/components/BtnLogout';
 
 export const Header = () => {
 
+    const isConnect = useAtomValue(isConnectAtom);
+
     return (
-        <header className="flex justify-between items-center py-4 px-8 bg-main-100">
-            <div className="flex items-center gap-4">
-                <img className="w-12" src="/icons/producktivity_logo.svg" alt="logo du site Pro'Duck'Tivity" />
+        <header className="flex justify-between items-center py-4 px-8 bg-main-100 ">
+
+            <div className="items-center gap-4 hidden lg:flex">
+                <img className="w-12" src="/icons/producktivity_logo.svg" alt="logo du site Pro'Duck'Tivity représentant un canard" />
+
                 <p className="text-main-800 text-2xl uppercase font-bold font-chewy tracking-widest">Pro'<span className="text-secondary-400">Duck</span>'Tivity</p>
             </div>
 
-            <nav>
-
-                <ul className="flex items-center gap-4 text-lg font-bold">
+            <nav className='flex flex-row justify-between gap-10 w-full lg:w-auto'>
+                <ul className="flex items-center gap-6 text-lg font-bold">
                     <li>
-                        <NavLink to="/">Accueil</NavLink>
+                        <NavLink to="/" className='whitespace-nowrap'>Accueil</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/tasks">Tâches</NavLink>
+                        <NavLink to="/tasks" className='whitespace-nowrap'>Tâches</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/promodoro">Pomodoro</NavLink>
+                        <NavLink to="/pomodoro" className='whitespace-nowrap'>Pomodoro</NavLink>
                     </li>
                     <li>
-                        <NavLink to="about" >À propos</NavLink>
+                        <NavLink to="/faq" className='whitespace-nowrap'>Faq</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/faq" >FAQ</NavLink>
+                        <NavLink to="/about" className='whitespace-nowrap'>A propos</NavLink>
                     </li>
+                </ul>
+                <ul>
                     <li>
-                        <NavLink className="btn" to="/auth/login">Me connecter</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="btn" to="/auth/register" >Créer un compte</NavLink>
+                        {!isConnect ? (
+                            <div className='btn-grp'>
+                                <NavLink className="btn flex flex-row gap-0.5" to="/auth/login">
+                                    <span>🐤</span>
+                                    <span className='whitespace-nowrap hidden lg:block'>Me connecter</span>
+                                </NavLink>
+                                <NavLink className="btn flex flex-row gap-0.5" to="/auth/register">
+                                    <span>🐣</span>
+                                    <span className='whitespace-nowrap hidden lg:block'>Créer un compte</span>
+                                </NavLink>
+                            </div>
+                        ) : (
+                            <BtnLogout />
+                        )}
                     </li>
                 </ul>
 
