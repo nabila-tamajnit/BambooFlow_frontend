351633297f3b9a505893d6be9da9258b4e98372b
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-04-09 01:28:30 +0200

    update : app name

--- src/layout/components/Footer.jsx
+++ src/layout/components/Footer.jsx
@@ -2,7 +2,7 @@
 export const Footer = () => {
     return (
         <footer className="bg-main-100 py-4 px-8 flex justify-between items-center">
-            <p className="text-main-800"> &copy; Pro'Duck'Tivity - 2026 - Tous droits réservés </p>
+            <p className="text-main-800"> &copy; BambooFlow - 2026 - Tous droits réservés </p>
 
             <div>
                 <a className="text-secondary-500 transition duration-100 hover:text-secondary-700 underline" href="">Conditions générales</a>
