21dc522fe4944129955e04f2333130f49507a09c
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-04-09 01:26:44 +0200

    change noFound image

--- src/layout/pages/NotFound.jsx
+++ src/layout/pages/NotFound.jsx
@@ -5,9 +5,9 @@ export const NotFound = () => {
         <section className="flex flex-col items-center justify-center gap-6 py-4 grow">
             <h1 className="text-9xl text-main-500 drop-shadow-sm font-extrabold font-chewy">404</h1>
 
-            <h2 className="text-lg text-main-800">La ressource que vous cherchez n'existe pas</h2>
+            <h2 className="text-lg text-main-700">La ressource que vous cherchez n'existe pas</h2>
 
-            <img className="h-56" src="/images/duck_404.png" alt="image de canard cubique" />
+            <img className="h-56" src="/images/panda_404.svg" alt="image de panda" />
         </section>
     )
 }
