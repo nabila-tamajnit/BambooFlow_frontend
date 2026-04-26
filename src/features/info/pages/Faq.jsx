dc7542452a71f678e0a90ee6341c91d1e49a7f10
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-04-26 21:10:52 +0200

    padding adjustment

--- src/features/info/pages/Faq.jsx
+++ src/features/info/pages/Faq.jsx
@@ -4,13 +4,13 @@ import faqs from '../data/faq.json';
 export const Faq = () => {
     return (
         <>
-            <section className="py-6 px-12 flex items-center gap-6">
+            <section className="py-6 px-4 sm:px-12 flex items-center gap-6">
                 <h1 className="text-3xl text-main-800 font-chewy">
                     La foire aux questions est ici !
                 </h1>
             </section>
 
-            <section className="flex flex-col px-12 gap-4 pb-12">
+            <section className="flex flex-col px-4 sm:px-12 gap-4 pb-12">
                 {faqs.map(faq => (
                     <FaqItem key={faq.id} {...faq} />
                 ))}
